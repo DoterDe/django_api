dcf2d0502d17007fb19112b646c5ec320b0dd879
Чилманов Абдуррохман Асанович <ChilmanovA@lcoast.itstep>
2024-09-11 22:41:59 +0500

    qwert

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from rest_framework.decorators import api_view
+
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
@@ -10,36 +11,60 @@ from .models import UserProfile,Product
 from django.views.generic.edit import CreateView, DeleteView
 from .serializers import ProductSerializers
 from django.http import JsonResponse
-from rest_framework import status
+from rest_framework import status, generics
+from rest_framework.viewsets import ModelViewSet,ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-@api_view(['GET', 'POST'])
-def api_product (request):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializers(products, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = ProductSerializers(data=request.data) 
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data,status=status.HTTP_201_CREATED)
-    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE']) 
-def api_product_id(request, pk): 
-    rubric = Product.objects.get(pk=pk) 
-    if request.method == 'GET': 
-        serializer = ProductSerializers(rubric) 
-        return Response(serializer.data) 
-    elif request.method == 'PUT' or request.method == "PATCH": 
-        serializer = ProductSerializers(rubric, data=request.data) 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
-    elif request.method == 'DELETE': 
-        rubric.delete() 
-        return Response (status=status.HTTP_204_NO_CONTENT)
+@permission_classes((IsAuthenticated,))
+class APIProductViewSet(ModelViewSet):
+    queryset =Product.objects.all()
+    serializer_class = ProductSerializers
+
+
+class APIREADProductViewSet(ReadOnlyModelViewSet):
+    queryset =Product.objects.all()
+    serializer_class = ProductSerializers
+    permission_classes = (IsAuthenticated,)
+
+# class APIProduct(generics.ListAPIView):
+#     queryset =  Product.objects.all()
+#     serializer_class = ProductSerializers
+
+# class APIProductDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset =  Product.objects.all()
+#     serializer_class = ProductSerializers
+
+
+
+# @api_view(['GET', 'POST'])
+# def api_product (request):
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         serializer = ProductSerializers(products, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = ProductSerializers(data=request.data) 
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data,status=status.HTTP_201_CREATED)
+#     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['GET', 'PUT', 'PATCH', 'DELETE']) 
+# def api_product_id(request, pk): 
+#     rubric = Product.objects.get(pk=pk) 
+#     if request.method == 'GET': 
+#         serializer = ProductSerializers(rubric) 
+#         return Response(serializer.data) 
+#     elif request.method == 'PUT' or request.method == "PATCH": 
+#         serializer = ProductSerializers(rubric, data=request.data, partial = True) 
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+#     elif request.method == 'DELETE': 
+#         rubric.delete() 
+#         return Response ('Success delete',status=status.HTTP_204_NO_CONTENT)
 
 # @api_view(['GET', 'POST'])
 # def api_rubrics(request) : 
@@ -49,7 +74,9 @@ def api_product_id(request, pk):
 
 def home(request):
     posts=UserProfile.objects.all()
-
+    if request.method == 'POST':
+        api_id = request.POST['id_product']
+        return redirect(f'api_id/{api_id}')
 
     return render(request, 'home.html', context= {'posts':posts } )
 
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,15 @@
+
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
-from app.views import email_massage,home, DeletePost, CreatePost,api_product,api_product_id
+from django.urls import path,include
+from app.views import email_massage,home, DeletePost, CreatePost,APIProductViewSet,APIREADProductViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('product', APIProductViewSet)
+router1 = DefaultRouter()
+router1.register('product1', APIREADProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,7 +17,10 @@ urlpatterns = [
     path('', home, name='home'),
     path('create/', CreatePost.as_view(), name='create'),
     path('delete/<int:id>', DeletePost.as_view(), name='delete'),
-    path('api/',api_product, name='api' ),
-    path('api_id/<int:pk>/', api_product_id, name='api_id')
-
+    # path('api/',api_product, name='api' ),
+    # path('api_id/<int:pk>/', api_product_id, name='api_id'),
+    path('api_product/', include(router.urls)),
+    path('api_read', include(router1.urls)),
+    # path('api/',APIProduct.as_view(), name='api' ),
+    # path('api_id/<int:pk>/', APIProductDetail.as_view(), name='api_id')
 ]
