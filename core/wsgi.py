824821db49082815ba40a8500fb88e1853ff60e9
Чилманов Абдуррохман Асанович <ChilmanovA@lcoast.itstep>
2024-09-20 20:55:39 +0500

    commit

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/myusername/mysite'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = get_wsgi_application()
